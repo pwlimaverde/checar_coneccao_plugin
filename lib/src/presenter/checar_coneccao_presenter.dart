07c2dd42735e36e74508a84d9e5ee1358c02129b
PWLIMAVERDE <vorfast@gmail.com>
2021-02-06 16:42:36 -0300

    criação do tempo de execução

--- lib/src/presenter/checar_coneccao_presenter.dart
+++ lib/src/presenter/checar_coneccao_presenter.dart
@@ -1,3 +1,4 @@
+import 'package:checar_coneccao_plugin/src/utilitarios/tempo_execucao.dart';
 import 'package:connectivity/connectivity.dart';
 import 'package:retorno_sucesso_ou_erro_package/retorno_sucesso_ou_erro_package.dart';
 
@@ -7,10 +8,13 @@ import '../datasources/connectivity_datasource.dart';
 
 class ChecarConeccaoPresenter {
   final Connectivity? connectivity;
+  final bool? mostrarTempoExecucao;
 
-  ChecarConeccaoPresenter({this.connectivity});
+  ChecarConeccaoPresenter({this.connectivity, this.mostrarTempoExecucao});
 
   Future<RetornoSucessoOuErro<bool>> consultaConectividade() async {
+    TempoExecucao tempo = TempoExecucao();
+    tempo.iniciar();
     RetornoSucessoOuErro<bool> resultado = await ChecarConeccaoUsecase(
       repositorio: ChecarConeccaoRepositorio(
         datasource: ConnectivityDatasource(
@@ -18,6 +22,11 @@ class ChecarConeccaoPresenter {
         ),
       ),
     )(parametros: NoParams());
+    if (mostrarTempoExecucao ?? false) {
+      tempo.terminar();
+      print(
+          "Tempo de Execução do ChecarConeccaoPresenter: ${tempo.calcularExecucao()}ms");
+    }
     return resultado;
   }
 }
